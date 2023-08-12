9b0df1e9733ef1fecccec9868c0fe96b1c666a4f
CocoRoF <gkfua00@gmail.com>
2023-08-12 11:14:10 +0900

    0811

--- util/chain_model.py
+++ util/chain_model.py
@@ -1,41 +1,35 @@
 from util.module import *
 
-def sentiment_chain_model(text:str, show_sentiment:bool=False, show_emotion:bool=False):
+def sentiment_chain_model(text:str, show_sentiment:bool=False, emotion:bool=False, show_emotion:bool=False, senti_tempe:float=0, emot_tempe:float=0, chat_tempe:float=0.8, verbose:bool=False, load_memory='Default'):
   user_chat = text
-  sentiment = sentiment_chat(user_chat)['sentiment']
-  emotion = emotion_chat(user_chat)['emotion']
+  sentiment = sentiment_chat(user_chat, temperature=senti_tempe)['sentiment']
+  if emotion:
+    user_emotion = emotion_chat(user_chat, temperature=emot_tempe)['emotion']
+    if sentiment == 'Positive':
+      answer = positive_chat(user_chat, emotion = emotion, temperature=chat_tempe, emot_tempe=emot_tempe, verbose=verbose, load_memory=load_memory)
 
-  if sentiment == ('Very Positive') or ('Positive'):
-    answer = positive_chat(user_chat)
+    else:
+      answer = negative_chat(user_chat, emotion = emotion, temperature=chat_tempe, emot_tempe=emot_tempe, verbose=verbose, load_memory=load_memory)
+
+    result_dict = {}
+    if show_sentiment:
+        result_dict['Sentiment'] = sentiment
+    if show_emotion:
+        result_dict['Emotion'] = user_emotion
+    result_dict['Answer'] = answer
+
+    return result_dict
 
   else:
-    answer = negative_chat(user_chat)
+    if sentiment == 'Positive':
+      answer = positive_chat(user_chat, temperature=chat_tempe, emot_tempe=emot_tempe, verbose=verbose, load_memory=load_memory)
 
-  result_dict = {}
-  if show_sentiment:
-      result_dict['Sentiment'] = sentiment
-  if show_emotion:
-      result_dict['Emotion'] = emotion
-  result_dict['Answer'] = answer
+    else:
+      answer = negative_chat(user_chat, temperature=chat_tempe, emot_tempe=emot_tempe, verbose=verbose, load_memory=load_memory)
 
-  return result_dict
+    result_dict = {}
+    if show_sentiment:
+        result_dict['Sentiment'] = sentiment
+    result_dict['Answer'] = answer
 
-def sentiment_chain_model(text:str, show_sentiment:bool=False, show_emotion:bool=False):
-  user_chat = text
-  sentiment = sentiment_chat(user_chat)['sentiment']
-  emotion = emotion_chat(user_chat)['emotion']
-
-  if sentiment == ('Very Positive') or ('Positive'):
-    answer = positive_chat(user_chat)
-
-  else:
-    answer = negative_chat(user_chat)
-
-  result_dict = {}
-  if show_sentiment:
-      result_dict['Sentiment'] = sentiment
-  if show_emotion:
-      result_dict['Emotion'] = emotion
-  result_dict['Answer'] = answer
-
-  return result_dict
+    return result_dict
